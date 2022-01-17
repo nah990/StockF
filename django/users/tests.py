33498c6da8d6acd9416a785d87ea0548a817f362
nah990 <fire990q@gmail.com>
2022-01-17 17:03:16 +0300

    After unit-testing impl build

--- django/users/tests.py
+++ django/users/tests.py
@@ -2,34 +2,42 @@ from django.test import TestCase
 from .models import CustomUser, CustomUserManager
 from stock.db_manager import DBConfigManager
 from stock.repositories import CustomUserRepository
+from stock.repository_pattern import Repository
 from stock.services import CustomService, CustomUserService
 from core.dataBuilder import *
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock, Mock, patch
 
 class TestDBRoleConnection(TestCase):
 
     databases = '__all__'
-    db_config_manager = DBConfigManager()
 
     @staticmethod
     def test_admin_connection():
         client_user = AdminUser().user
-        assert(TestDBRoleConnection.db_config_manager.get_connection(client_user) == 'admin_role_connect')
+        db_config_manager = DBConfigManager()
+        connected_role = db_config_manager.get_connection(client_user)
+        assert(connected_role == 'admin_role_connect')
 
     @staticmethod
     def test_superuser_connection():
         client_user = SuperUser().user
-        assert(TestDBRoleConnection.db_config_manager.get_connection(client_user) == 'admin_role_connect')
+        db_config_manager = DBConfigManager()
+        connected_role = db_config_manager.get_connection(client_user)
+        assert(connected_role == 'admin_role_connect')
 
     @staticmethod
     def test_user_connection():
         client_user = User().user
-        assert(TestDBRoleConnection.db_config_manager.get_connection(client_user) == 'user_role_connect')
+        db_config_manager = DBConfigManager()
+        connected_role = db_config_manager.get_connection(client_user)
+        assert(connected_role == 'user_role_connect')
 
     @staticmethod
     def test_specialist_connection():
         client_user = SpecialistUser().user
-        assert(TestDBRoleConnection.db_config_manager.get_connection(client_user) == 'specialist_role_connect')
+        db_config_manager = DBConfigManager()
+        connected_role = db_config_manager.get_connection(client_user)
+        assert(connected_role == 'specialist_role_connect')
 
     
 class TestCustomUserRepository(TestCase):
@@ -43,11 +51,21 @@ class TestCustomUserRepository(TestCase):
         mock.save.assert_called_with(using=CustomUserRepository.db_config_manager.get_connection(TestCustomUserRepository.client_user))
 
     @staticmethod
-    def test_admin_CRUD():
+    def test_admin_create():
+        user = AdminUser().user
+
+        create_flag = 1
+        try:
+            CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+        except:
+            create_flag = 0
+        assert(create_flag == 1)
+
+    @staticmethod
+    def test_admin_read():
         user = AdminUser().user
 
         CustomUserRepository.create(TestCustomUserRepository.client_user, user)
-
 
         db_user = CustomUserRepository.read_by_pk(TestCustomUserRepository.client_user, 1)
 
@@ -58,11 +76,23 @@ class TestCustomUserRepository(TestCase):
         assert(db_user.password == "secure_password")
         assert(db_user.login == "nah")
 
+    @staticmethod
+    def test_admin_update():
+        user = AdminUser().user
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+
         CustomUserRepository.update_by_pk(TestCustomUserRepository.client_user, 1, {'email': "testing@mail.ru", 'login' : "testing"})
         db_user = CustomUserRepository.read_by_pk(TestCustomUserRepository.client_user, 1)
 
         assert(db_user.email == "testing@mail.ru")
         assert(db_user.login == "testing")
+
+    @staticmethod
+    def test_admin_delete():
+        user = AdminUser().user
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
 
         CustomUserRepository.delete_by_pk(TestCustomUserRepository.client_user, 1)
         delete_flag = 0
@@ -73,7 +103,7 @@ class TestCustomUserRepository(TestCase):
         assert(delete_flag == 1)
 
     @staticmethod
-    def test_user_CRUD():
+    def test_user_create():
         user = CustomUser(
         pk = 1,
         email = "1234@mail.ru",
@@ -82,9 +112,24 @@ class TestCustomUserRepository(TestCase):
         login = "1234qwerty"
         )
 
- 
-        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+        create_flag = 1
+        try:
+            CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+        except:
+            create_flag = 0
+        assert(create_flag == 1)
 
+    @staticmethod
+    def test_user_read():
+        user = CustomUser(
+        pk = 1,
+        email = "1234@mail.ru",
+        password = "secure_password",
+        role = 0,
+        login = "1234qwerty"
+        )
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
 
         db_user = CustomUserRepository.read_by_pk(TestCustomUserRepository.client_user, 1)
 
@@ -95,11 +140,35 @@ class TestCustomUserRepository(TestCase):
         assert(db_user.password == "secure_password")
         assert(db_user.login == "1234qwerty")
 
+    @staticmethod
+    def test_user_update():
+        user = CustomUser(
+        pk = 1,
+        email = "1234@mail.ru",
+        password = "secure_password",
+        role = 0,
+        login = "1234qwerty"
+        )
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+
         CustomUserRepository.update_by_pk(TestCustomUserRepository.client_user, 1, {'email': "updated@mail.ru", 'login' : "updated"})
         db_user = CustomUserRepository.read_by_pk(TestCustomUserRepository.client_user, 1)
 
         assert(db_user.email == "updated@mail.ru")
         assert(db_user.login == "updated")
+
+    @staticmethod
+    def test_user_delete():
+        user = CustomUser(
+        pk = 1,
+        email = "1234@mail.ru",
+        password = "secure_password",
+        role = 0,
+        login = "1234qwerty"
+        )
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
 
         CustomUserRepository.delete_by_pk(TestCustomUserRepository.client_user, 1)
         delete_flag = 0
@@ -110,7 +179,7 @@ class TestCustomUserRepository(TestCase):
         assert(delete_flag == 1)
     
     @staticmethod
-    def test_specialist_CRUD():
+    def test_specialist_create():
         user = CustomUser(
         pk = 1,
         email = "test@mail.ru",
@@ -120,9 +189,26 @@ class TestCustomUserRepository(TestCase):
         is_staff = True
         )
 
- 
-        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+        create_flag = 1
+        try:
+            CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+        except:
+            create_flag = 0
+        assert(create_flag == 1)
 
+    
+    @staticmethod
+    def test_specialist_read():
+        user = CustomUser(
+        pk = 1,
+        email = "test@mail.ru",
+        password = "secure_password",
+        role = 1,
+        login = "testqwerty",
+        is_staff = True
+        )
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
 
         db_user = CustomUserRepository.read_by_pk(
         TestCustomUserRepository.client_user, 1)
@@ -134,11 +220,39 @@ class TestCustomUserRepository(TestCase):
         assert(db_user.password == "secure_password")
         assert(db_user.login == "testqwerty")
 
+
+    @staticmethod
+    def test_specialist_update():
+        user = CustomUser(
+        pk = 1,
+        email = "test@mail.ru",
+        password = "secure_password",
+        role = 1,
+        login = "testqwerty",
+        is_staff = True
+        )
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
+
         CustomUserRepository.update_by_pk(TestCustomUserRepository.client_user, 1, {'email': "changed@mail.ru", 'login' : "changed"})
         db_user = CustomUserRepository.read_by_pk(TestCustomUserRepository.client_user, 1)
 
         assert(db_user.email == "changed@mail.ru")
         assert(db_user.login == "changed")
+
+
+    @staticmethod
+    def test_specialist_delete():
+        user = CustomUser(
+        pk = 1,
+        email = "test@mail.ru",
+        password = "secure_password",
+        role = 1,
+        login = "testqwerty",
+        is_staff = True
+        )
+
+        CustomUserRepository.create(TestCustomUserRepository.client_user, user)
 
         CustomUserRepository.delete_by_pk(TestCustomUserRepository.client_user, 1)
         delete_flag = 0
@@ -147,22 +261,3 @@ class TestCustomUserRepository(TestCase):
         except:
             delete_flag = 1
         assert(delete_flag == 1)
-
-
-class TestCustomUserService(TestCase):
-
-    @staticmethod
-    def test_create_method_service_dependency_with_repository():
-        user = AdminUser().user
-        mock = MagicMock()
-        customUserService = CustomService(mock)
-        customUserService.create(user, user)
-        mock.create.assert_called_with(user, user)
-
-    @staticmethod
-    def test_read_by_pk_method_service_dependency_with_repository():
-        user = AdminUser().user
-        mock = MagicMock()
-        customUserService = CustomService(mock)
-        customUserService.read_by_pk(user, 1)
-        mock.read_by_pk.assert_called_with(user, 1)
