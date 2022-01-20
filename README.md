22a189938776556f48138293de3db5f309c9e884
nah990 <fire990q@gmail.com>
2022-01-20 20:50:23 +0300

    Added first front impl. Splited docker(Multi&Solo backend version)

--- README.md
+++ README.md
@@ -16,9 +16,15 @@ git clone https://github.com/nah990/StockF.git
 <br />
 
 Make sure you have Docker installed then write in dir.
-
+For version with one backend.
 ```
-docker-compose up -d --build
+docker-compose --profile solo-backend up -d --build
+```
+
+or
+For version with multiple backend to enable nginx rebalancing features.
+```
+docker-compose --profile multi-backend up -d --build
 ```
 
 After that just visit
---
+++ nginx/static/precache-manifest.7d1943d48e2eb36326b088d636ce0e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3884980096cee85d3b5a4516a590bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9faa10b208848553761",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6df8f5f4d6dd9e35c480",
+    "url": "/static/js/2.438976eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.438976eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9faa10b208848553761",
+    "url": "/static/js/main.bbdc0bdc.chunk.js"
+  },
+  {
+    "revision": "ec0406a13c7cd822070d",
+    "url": "/static/js/runtime-main.e3a0d51c.js"
+  }
+]);
